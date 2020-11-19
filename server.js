ec5e8251ffcd1685bb32a3a28b5f6340ee77bcbd
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-19 23:11:31 +1100

    COnnect to DB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected!"))
+  .catch((error) => console.log("error:", error));
 const server = new ApolloServer({
   typeDefs,
   resolvers,
