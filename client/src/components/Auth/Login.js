a68cc5b591e1dc9db55987f2cfa6696dfedd6abb
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-20 12:43:45 +1100

    Authenticate users from Apollo server

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { GraphQLClient } from "graphql-request";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
-
+const ME_QUERY = `{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}`;
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async (googleUser) => {
+    console.log(googleUser);
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { authorization: id_token },
+    });
+    const data = await client.request(ME_QUERY);
+    console.log("data", data);
+  };
+  return (
+    <GoogleLogin
+      clientId="664012434037-4ub12eud5d77t7ic429bgmflejsvkln7.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+    // <div className="g-signin2" onsuccess={onSignIn} />
+  );
 };
 
 const styles = {
@@ -12,8 +38,8 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export default withStyles(styles)(Login);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const { findOrCreateUser } = require("./controllers/userController");
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("DB Connected!"))
@@ -10,5 +12,19 @@ mongoose
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        // find or create user
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with ${authToken}`);
+    }
+    return { currentUser };
+  },
 });
 server.listen().then(({ url }) => console.log(`Server is listening on ${url}`));
