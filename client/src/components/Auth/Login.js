07b20d7218d5ecb0710a9787ce306f035c0c0519
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-20 13:06:07 +1100

    adding reduc to store user state

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
+import Context from "../../context";
 const ME_QUERY = `{
   me {
     _id
@@ -12,6 +13,7 @@ const ME_QUERY = `{
   }
 }`;
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
   const onSuccess = async (googleUser) => {
     console.log(googleUser);
     const id_token = googleUser.getAuthResponse().id_token;
@@ -21,6 +23,7 @@ const Login = ({ classes }) => {
     });
     const data = await client.request(ME_QUERY);
     console.log("data", data);
+    dispatch({ type: "LOGIN_USER", payload: data.me });
   };
   return (
     <GoogleLogin
