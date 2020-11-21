31ac6722690a20033a71acc980e9367054249dd6
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-21 14:42:47 +1100

    create comments component + reducer

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -72,6 +72,17 @@ export default function reducer(state, { type, payload }) {
         ...state,
         pins: filteredPins,
       };
+    case "CREATE_COMMENT":
+      const updatedCurrentPin = payload;
+      // find and replace
+      const updatedPins = state.pins.map((pin) =>
+        pin._id === updatedCurrentPin._id ? updatedCurrentPin : pin,
+      );
+      return {
+        ...state,
+        pins: updatedPins,
+        currentPin: updatedCurrentPin,
+      };
     default:
       return state;
   }
