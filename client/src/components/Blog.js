8c09215501d18ff9d555eae40f47a44d35e18ba6
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-21 12:20:35 +1100

    blog for add pin

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import { Paper } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
+import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
+
+import Context from "../context";
+import NoContent from "./Pin/NoContent";
+import CreatePin from "./Pin/CreatePin";
+import PinContent from "./Pin/PinContent";
 
 const Blog = ({ classes }) => {
-  return <div>Blog</div>;
+  const mobileSize = useMediaQuery("(max-width: 650px)");
+  const { state } = useContext(Context);
+  const { draft, currentPin } = state;
+
+  let BlogContent;
+  if (!draft && !currentPin) {
+    BlogContent = NoContent;
+  } else if (draft && !currentPin) {
+    BlogContent = CreatePin;
+  } else if (!draft && currentPin) {
+    BlogContent = PinContent;
+  }
+
+  return (
+    <Paper className={mobileSize ? classes.rootMobile : classes.root}>
+      <BlogContent />
+    </Paper>
+  );
 };
 
 const styles = {
@@ -13,14 +36,14 @@ const styles = {
     maxHeight: "calc(100vh - 64px)",
     overflowY: "scroll",
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   rootMobile: {
     maxWidth: "100%",
     maxHeight: 300,
     overflowX: "hidden",
-    overflowY: "scroll"
-  }
+    overflowY: "scroll",
+  },
 };
 
 export default withStyles(styles)(Blog);
