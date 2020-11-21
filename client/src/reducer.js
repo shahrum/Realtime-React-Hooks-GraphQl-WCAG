94b5c77834e6b5f995c95302788b1ef925468a16
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-21 12:07:15 +1100

    add GET_PINS action

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -16,6 +16,30 @@ export default function reducer(state, { type, payload }) {
         isAuth: false,
         currentUser: null,
       };
+    case "CREATE_DRAFT":
+      return {
+        ...state,
+        currentPin: null,
+        draft: {
+          latitude: 0,
+          longitude: 0,
+        },
+      };
+    case "UPDATE_DRAFT_LOCATION":
+      return {
+        ...state,
+        draft: payload,
+      };
+    case "DELETE_DRAFT":
+      return {
+        ...state,
+        draft: null,
+      };
+    case "GET_PINS":
+      return {
+        ...state,
+        pins: payload,
+      };
     default:
       return state;
   }
