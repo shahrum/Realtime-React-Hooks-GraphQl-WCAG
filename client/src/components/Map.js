36ed58dc85b5b16f7493717343f478c904df20b4
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-21 12:20:52 +1100

    add map with mapbox

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,42 +1,163 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import differenceInMinutes from "date-fns/difference_in_minutes";
+import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
+
+import { useClient } from "../client";
+import { GET_PINS_QUERY } from "../graphql/queries";
+import PinIcon from "./PinIcon";
+import Blog from "./Blog";
+import Context from "../context";
+
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13,
+};
 
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const client = useClient();
+  const mobileSize = useMediaQuery("(max-width: 650px)");
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    getPins();
+  }, []);
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
+
+  const getPins = async () => {
+    const { getPins } = await client.request(GET_PINS_QUERY);
+    dispatch({ type: "GET_PINS", payload: getPins });
+  };
+
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: "CREATE_DRAFT" });
+    }
+    const [longitude, latitude] = lngLat;
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude },
+    });
+  };
+
+  const highlightNewPin = (pin) => {
+    const isNewPin =
+      differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30;
+    return isNewPin ? "limegreen" : "darkblue";
+  };
+
+  return (
+    <div className={mobileSize ? classes.rootMobile : classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1Ijoic2hhaHJhbTE5ODkiLCJhIjoiY2tocXZjNXV3MDc3bzJ5cW1hMXoydXl4YyJ9.SvWd8XSwcz9qzAOT7X-Ejw"
+        scrollZoom={!mobileSize}
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+        onClick={handleMapClick}
+        {...viewport}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
+
+        {/* Draft Pin */}
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
+          </Marker>
+        )}
+
+        {/* Created Pins */}
+        {state.pins.map((pin) => (
+          <Marker
+            key={pin._id}
+            latitude={pin.latitude}
+            longitude={pin.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon
+              // onClick={() => handleSelectPin(pin)}
+              size={40}
+              color={highlightNewPin(pin)}
+            />
+          </Marker>
+        ))}
+      </ReactMapGL>
+
+      {/* Blog Area to add Pin Content */}
+      <Blog />
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: "flex",
   },
   rootMobile: {
     display: "flex",
-    flexDirection: "column-reverse"
+    flexDirection: "column-reverse",
   },
   navigationControl: {
     position: "absolute",
     top: 0,
     left: 0,
-    margin: "1em"
+    margin: "1em",
   },
   deleteIcon: {
-    color: "red"
+    color: "red",
   },
   popupImage: {
     padding: "0.4em",
     height: 200,
     width: 200,
-    objectFit: "cover"
+    objectFit: "cover",
   },
   popupTab: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column"
-  }
+    flexDirection: "column",
+  },
 };
 
 export default withStyles(styles)(Map);
