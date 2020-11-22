474a4764b22c2c65a47cd7484352ebd694423269
shahram.hosseinabadi <shahram.hosseinabadi@marlo.com.au>
2020-11-22 11:03:10 +1100

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ import { WebSocketLink } from "apollo-link-ws";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const webSocketLink = new WebSocketLink({
-  uri: "ws://localhost:4000/graphql",
+  uri: "wss://realtime-react.herokuapp.com/graphql",
   options: {
     reconnect: true,
   },
@@ -50,4 +50,4 @@ ReactDOM.render(<Root />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
